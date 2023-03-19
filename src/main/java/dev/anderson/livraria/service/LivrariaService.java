b860972ac79aef0645788a38fccb5356400dfbab
Anderson Porto <anderson.higo2@gmail.com>
2023-03-19 13:53:20 -0300

    fix: using BookNotFoundException

--- src/main/java/dev/anderson/livraria/service/LivrariaService.java
+++ src/main/java/dev/anderson/livraria/service/LivrariaService.java
@@ -1,6 +1,7 @@
 package dev.anderson.livraria.service;
 
 import dev.anderson.livraria.entity.LivroEntity;
+import dev.anderson.livraria.exception.BookNotFoundException;
 import dev.anderson.livraria.exception.DuplicatedBookException;
 import dev.anderson.livraria.repository.LivrariaRepository;
 import java.util.List;
@@ -28,21 +29,21 @@ public class LivrariaService {
 
   public LivroEntity getBookByNameAndYear(String name, Integer year) {
     if (!livrariaRepository.existsByNameAndYear(name, year)) {
-      throw new DuplicatedBookException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
+      throw new BookNotFoundException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
     }
     return livrariaRepository.findByNameAndYear(name, year);
   }
 
   public void deleteBook(Long id) {
     if (!livrariaRepository.existsById(id)) {
-      throw new DuplicatedBookException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
+      throw new BookNotFoundException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
     }
     livrariaRepository.deleteById(id);
   }
 
   public LivroEntity updateBook(Long id, LivroEntity livro) {
     if (!livrariaRepository.existsById(id)) {
-      throw new DuplicatedBookException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
+      throw new BookNotFoundException(HttpStatus.BAD_REQUEST, "Livro não encontrado");
     }
     LivroEntity livroEntity = livrariaRepository.findById(id).get();
 
